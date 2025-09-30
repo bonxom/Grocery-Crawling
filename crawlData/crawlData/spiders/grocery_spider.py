1f7b6f1c5459e9ed52cf4d98f8f4e3a94608d914
Ho Minh Dung <bonxom@gmail.com>
2025-10-01 00:55:53 +0700

    Simple crawling

--- crawlData/crawlData/spiders/grocery_spider.py
+++ crawlData/crawlData/spiders/grocery_spider.py
@@ -6,3 +6,17 @@ class GrocerySpider(scrapy.Spider):
     name = "grocery"
     allowed_domains = ["webtaphoa.vn"]
     start_urls = ["https://www.webtaphoa.vn/collections/all"]
+
+    def parse(self, response):
+        list_products = response.css('div.product-item')
+        
+        for product in list_products:
+            yield {
+                'name': product.css('h3.pro-name a::attr(title)').get(),
+                'price': product.css('span.current-price::text').get(default="").replace('₫', '').strip(),
+                'link': response.urljoin(product.css('h3.pro-name a::attr(href)').get()),
+            }
+
+        next_page = response.css('a.next').attrib['href']
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
